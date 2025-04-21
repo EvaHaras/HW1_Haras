7f50e5ce38323b33d890157e6c6fbba37f2541ae
RoudePH <sgaras@gerc.ua>
2025-04-21 22:47:19 +0300

    HW22

---
+++ HW_nodeJS_haras/HW22/app.js
@@ -0,0 +1,19 @@
+const { exec } = require('child_process');
+
+const command = process.platform === 'win32' ? 'dir' : 'ls -la';
+
+console.log(`Запуск команди: ${command}\n`);
+
+exec(command, (error, stdout, stderr) => {
+    if (error) {
+        console.error(` Помилка виконання: ${error.message}`);
+        return;
+    }
+
+    if (stderr) {
+        console.error(` Стандартна помилка: ${stderr}`);
+        return;
+    }
+
+    console.log(` Результат виконання:\n${stdout}`);
+});
